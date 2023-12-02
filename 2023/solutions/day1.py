af00b410cca2f9a392fd37a1011c1fdff8743eaf
aashishred <95857097+aashishred@users.noreply.github.com>
2023-12-02 01:27:33 +0000

    Add files via upload

---
+++ 2023/solutions/day1.py
@@ -0,0 +1,42 @@
+import time, tracemalloc
+
+tracemalloc.start()
+start = time.time()
+
+def partOne():
+    numbers = []
+    with open("day1.txt") as f:
+        for line in f.readlines():
+            lineNumbers = [char for char in line if char.isnumeric()]
+            calibrationValue = lineNumbers[0] + lineNumbers[-1]
+            numbers.append(int(calibrationValue))
+    return sum(numbers)      
+
+def partTwo():
+    numbers = []
+    spelledNumbers = {
+        "one":"on1e",
+        "two":"tw2o",
+        "three":"thr3ee",
+        "four":"fo4ur", 
+        "five":"fi5ve", 
+        "six":"si6x", 
+        "seven":"sev7en", 
+        "eight":"eig8ht", 
+        "nine":"ni9ne"
+    }
+    with open("day1.txt") as f:
+        for line in f.readlines():
+            for spelling, embeddedNumber in spelledNumbers.items():
+                line = line.replace(spelling, embeddedNumber)
+            lineNumbers = [char for char in line if char.isnumeric()]
+            calibrationValue = lineNumbers[0] + lineNumbers[-1]
+            numbers.append(int(calibrationValue))
+    return sum(numbers)    
+
+print("Part 1:", partOne())
+print("Part 2:", partTwo())
+
+print("Elapsed time", time.time()-start)
+print("Memory usage:", tracemalloc.get_traced_memory()[0], " Peak: ", tracemalloc.get_traced_memory()[1])
+tracemalloc.stop()
